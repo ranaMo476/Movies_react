a8b1a1d008d8932b4c028c7032581824488fb48f
Rana <rana.mohattia55@gmail.com>
2023-10-03 15:38:05 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./components/Card";
 import './App.css';
 import image from './2.jpg';
@@ -9,15 +9,32 @@ import Main from "./components/Main";
  
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import { useRef } from "react";
+import { createContext } from "react";
 
+export const exusecontext = createContext()
 function App() {
   const [increase,setincrease]=useState(0)
+  const [x,setx]=useState(0)
+
+  useEffect(()=>{
+    console.log("test use effect")
+  },[x,increase])
+
+  const r = useRef(null)
+
+
+  useEffect(()=>{
+   r.current.focus();
+  },[])
+
   const increasevalue =()=>{
     setincrease(increase+1);
   }
   const decreasevalue =()=>{
     setincrease(increase-1);
   }
+
   const arr =[{title:"MOHAMED1",imag:'1.jpg'},{title:"MOHAMED9",imag:"1.jpg"},{title:"HISHAM"} ]
   const click =()=>{console.log("print test prop unction");}
  const value_onchange_input =(e)=>{
@@ -49,8 +66,22 @@ function App() {
     <div className="useState_hook">
       <button onClick={increasevalue} >+</button>
       <button onClick={decreasevalue}>-</button>
+
+      <button onClick={()=>setx(x+2)} >+</button>
+      <button onClick={()=>setx(x-2)}>-</button>
+      {x}
       {increase}
+      <br/>
+      <br/>
+      <input ref={r}/>
+      <br/>
+      <br/>
+      
     </div>
+
+    <exusecontext.Provider value="movies project">
+          <Navbar/>
+      </exusecontext.Provider>
     </div>        
     
  
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,18 @@
 import React from "react";
+import {exusecontext} from "../App"
+import { useContext } from "react";
 
 const Navbar=()=>{
+  const x = useContext(exusecontext)
     return(
-      <div> this is navbar</div>
+  
+      <div> this is navbar
+         <>
+       <h1>{x}</h1>
+     </>
+      </div>
+      
+     
     );
 }
 
